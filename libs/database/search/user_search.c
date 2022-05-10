08dbdca7139539063e9063c04e56cf122418ca19
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-05-10 22:44:49 +0200

    [ADD] database can search a user by id or pseudo

---
+++ libs/database/search/user_search.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** Teams
+** File description:
+** user_searchs
+*/
+
+#include "../fields/user.h"
+#include <stdbool.h>
+#include <string.h>
+
+bool user_id_compare(const void *elem, const void *search)
+{
+    const user_t *u = (const user_t *)elem;
+    const id_t *id = (const id_t *)id;
+    if (u->id == *id)
+        return true;
+    return false;
+}
+
+bool user_pseudo_compare(const void *elem, const void *search)
+{
+    const user_t *u = (const user_t *)elem;
+    const char *pseudo = (const char *)pseudo;
+    if (strcmp(u->pseudo, pseudo) == 0)
+        return true;
+    return false;
+}
