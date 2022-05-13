cfe3812e4fb0c4165433a4aabe9ebf01c848c78d
aurelenc <aurele.nicolas@epitech.eu>
2022-05-13 14:26:03 +0200

    [ADD] Added reply codes

---
+++ server/include/reply_codes.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2022
+** Epi-MyTeams
+** File description:
+** reply_codes
+*/
+
+#ifndef REPLY_CODES_H_
+#define REPLY_CODES_H_
+
+typedef struct reply_code_s {
+    int code;
+    char *message;
+} reply_code_t;
+
+extern const reply_code_t reply_codes[];
+
+#define REPLY_SUCCESS 200
+#define REPLY_SERVER_ERROR 500
+#define REPLY_NOT_IMPLEMENTED 501
+
+#endif /* !REPLY_CODES_H_ */
