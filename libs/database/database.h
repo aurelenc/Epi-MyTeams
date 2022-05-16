23773e5625f373494113f5afd5024cf369189f4a
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-05-16 17:56:47 +0200

    [DOC-REF] Change documentation style for database and linked list library due to the Epitech C Codind Style

--- libs/database/database.h
+++ libs/database/database.h
@@ -26,149 +26,151 @@ typedef struct database_s
     llist_t *discussions;
 } database_t;
 
-/**
- * @brief It creates a database
- *
- * @return A pointer to a database_t struct.
- */
+///
+/// @brief It creates a database
+///
+/// @return A pointer to a database_t struct.
+///
 database_t *db_creation(void);
 
-/**
- * @brief It frees all the linked lists in the database
- *
- * @param database the database to destroy
- *
- * @return A pointer to a database_t struct.
- */
+///
+/// @brief It frees all the linked lists in the database
+///
+/// @param database the database to destroy
+///
+/// @return A pointer to a database_t struct.
+///
 database_t *db_destuction(database_t *database);
 
-/**
- * @brief Add a user to the database.
- *
- * @param db The database to add the user to.
- * @param user The user to add to the database.
- *
- * @return A boolean value.
- */
+///
+/// @brief Add a user to the database.
+///
+/// @param db The database to add the user to.
+/// @param user The user to add to the database.
+///
+/// @return A boolean value.
+///
 static inline bool db_add_user(database_t *db, user_t *user)
 {
     return llist_append(db->users, user);
 };
 
-/**
- * @brief It searches the database for a user with the given id
- *
- * @param db The database to search in.
- * @param id The id of the user to search for.
- *
- * @return A pointer to a user_t struct.
- */
+///
+/// @brief It searches the database for a user with the given id
+///
+/// @param db The database to search in.
+/// @param id The id of the user to search for.
+///
+/// @return A pointer to a user_t struct.
+///
 static inline user_t *db_search_user_by_id(database_t *db, const id_t id)
 {
     return llist_search(db->users, &user_id_compare, id);
 };
 
-/**
- * @brief It searches the database for a user with the given pseudo
- *
- * @param db the database
- * @param pseudo the pseudo of the user to search for
- *
- * @return A pointer to the user_t struct.
- */
-static inline user_t *db_search_user_by_pseudo(database_t *db, const char *pseudo)
+///
+/// @brief It searches the database for a user with the given pseudo
+///
+/// @param db the database
+/// @param pseudo the pseudo of the user to search for
+///
+/// @return A pointer to the user_t struct.
+///
+static inline user_t *db_search_user_by_pseudo(database_t *db,
+const char *pseudo)
 {
     return llist_search(db->users, &user_pseudo_compare, pseudo);
 };
 
-/**
- * @brief It deletes a user, with the given id, in the database
- *
- * @param db The database to search in.
- * @param id The id of the user to delete.
- *
- * @return A boolean value.
- */
+///
+/// @brief It deletes a user, with the given id, in the database
+///
+/// @param db The database to search in.
+/// @param id The id of the user to delete.
+///
+/// @return A boolean value.
+///
 static inline bool db_delete_user_by_id(database_t *db, const id_t id)
 {
     return llist_delif(db->users, &user_id_compare, id);
 }
 
-/**
- * @brief It deletes a user, with the given pseudo, in the database
- *
- * @param db the database.
- * @param pseudo the pseudo of the user to delete.
- *
- * @return A boolean value.
- */
-static inline bool db_delete_user_by_pseudo(database_t *db, const char *pseudo)
+///
+/// @brief It deletes a user, with the given pseudo, in the database
+///
+/// @param db the database.
+/// @param pseudo the pseudo of the user to delete.
+///
+/// @return A boolean value.
+///
+static inline bool db_delete_user_by_pseudo(database_t *db,
+const char *pseudo)
 {
     return llist_delif(db->users, &user_pseudo_compare, pseudo);
 }
 
-/**
- * @brief It adds a team to the database
- *
- * @param db The database to add the team to.
- * @param team The team to add to the database.
- *
- * @return A boolean value.
- */
+///
+/// @brief It adds a team to the database
+///
+/// @param db The database to add the team to.
+/// @param team The team to add to the database.
+///
+/// @return A boolean value.
+///
 static inline bool db_add_team(database_t *db, team_t *team)
 {
     return llist_append(db->teams, team);
 };
 
-/**
- * @brief It searches the database for a team with the given id
- *
- * @param db The database to search in.
- * @param id The id of the team to search for.
- *
- * @return A pointer to a team_t struct.
- */
+///
+/// @brief It searches the database for a team with the given id
+///
+/// @param db The database to search in.
+/// @param id The id of the team to search for.
+///
+/// @return A pointer to a team_t struct.
+///
 
 static inline team_t *db_search_team_by_id(database_t *db, const id_t id)
 {
     return llist_search(db->teams, &team_id_compare, id);
 };
 
-/**
- * @brief It searches the database for a team with the given name
- *
- * @param db The database to search in.
- * @param name The name of the team to search for.
- *
- * @return A pointer to a team_t struct.
- */
+///
+/// @brief It searches the database for a team with the given name
+///
+/// @param db The database to search in.
+/// @param name The name of the team to search for.
+///
+/// @return A pointer to a team_t struct.
+///
 static inline team_t *db_search_team_by_name(database_t *db, const char *name)
 {
     return llist_search(db->teams, &team_name_compare, name);
 };
 
-/**
- * @brief It deletes a team from the database by its id
- *
- * @param db The database to delete the team from.
- * @param id The id of the team to delete
- *
- * @return A boolean value.
- */
+///
+/// @brief It deletes a team from the database by its id
+///
+/// @param db The database to delete the team from.
+/// @param id The id of the team to delete
+///
+/// @return A boolean value.
+///
 static inline bool db_delete_team_by_id(database_t *db, const id_t id)
 {
     return llist_delif(db->teams, &team_id_compare, id);
 }
 
-/**
- * @brief It deletes the first team in the database whose name matches the given
- * name
- *
- * @param db The database to search in.
- * @param name The name of the team to search for.
- *
- * @return A boolean value.
- */
+///
+/// @brief It deletes the first team in the database whose name matches the
+/// given name
+///
+/// @param db The database to search in.
+/// @param name The name of the team to search for.
+///
+/// @return A boolean value.
+///
 static inline bool db_delete_team_by_name(database_t *db, const char *name)
 {
     return llist_delif(db->teams, &team_name_compare, name);
--- libs/database/search/database_search_functions.h
+++ libs/database/search/database_search_functions.h
@@ -11,42 +11,42 @@
 ** Users
 */
 
-/**
- * @brief It compares a user's id to a given id
- *
- * @param elem The element to compare.
- * @param search The value to search for.
- *
- * @return A boolean value.
- */
+///
+/// @brief It compares a user's id to a given id
+///
+/// @param elem The element to compare.
+/// @param search The value to search for.
+///
+/// @return A boolean value.
+///
 bool user_id_compare(const void *elem, const void *search);
 
-/**
- * @brief It compares the pseudo of a user with a given pseudo
- *
- * @param elem the element to compare
- * @param search the value to search for
- *
- * @return A boolean value.
- */
+///
+/// @brief It compares the pseudo of a user with a given pseudo
+///
+/// @param elem the element to compare
+/// @param search the value to search for
+///
+/// @return A boolean value.
+///
 bool user_pseudo_compare(const void *elem, const void *search);
 
-/**
- * @brief It compares the id of a team with the id of a user
- *
- * @param elem The element in the array to compare.
- * @param search The value to search for.
- *
- * @return A pointer to the team_t struct that has the id that was passed in.
- */
+///
+/// @brief It compares the id of a team with the id of a user
+///
+/// @param elem The element in the array to compare.
+/// @param search The value to search for.
+///
+/// @return A pointer to the team_t struct that has the id that was passed in.
+///
 bool team_id_compare(const void *elem, const void *search);
 
-/**
- * @brief It compares the name of a team to a string
- *
- * @param elem The element in the array to compare.
- * @param search The value to search for.
- *
- * @return A pointer to the team_t struct.
- */
+///
+/// @brief It compares the name of a team to a string
+///
+/// @param elem The element in the array to compare.
+/// @param search The value to search for.
+///
+/// @return A pointer to the team_t struct.
+///
 bool team_name_compare(const void *elem, const void *search);
