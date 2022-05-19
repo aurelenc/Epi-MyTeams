611240b7d04ecb250169dcac92692ee5a157e1a2
aurelenc <aurele.nicolas@epitech.eu>
2022-05-19 15:00:12 +0200

    [DEBUG] Added debug code to login and logout

--- server/src/commands/login.c
+++ server/src/commands/login.c
@@ -7,8 +7,11 @@
 
 #include "reply_codes.h"
 #include "server.h"
+#include <stdio.h>
 
 int command_login(command_param_t *param)
 {
+    printf("login\n");
+    write_client_buff(param->clients, param->id, "logged in\n");
     return REPLY_NOT_IMPLEMENTED;
 }
