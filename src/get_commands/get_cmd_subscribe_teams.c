48f37beae0694311fd841db2dd9e7cdfdcf4febe
Tom Duval-Mahe <tomduvalmahe44@gmail.com>
2022-05-19 16:48:26 +0200

    Delete File Gartbage and include Garbage

--- src/get_commands/get_cmd_subscribe_teams.c
+++
@@ -1,7 +0,0 @@
-/*
-** EPITECH PROJECT, 2022
-** My_Teams
-** File description:
-** get_cmd_subscribe_teams
-*/
-
--- src/get_commands/get_use_commands/get_cmd_use_teams.c
+++
@@ -1,7 +0,0 @@
-/*
-** EPITECH PROJECT, 2022
-** My_Teams
-** File description:
-** get_cmd_use_teams
-*/
-
