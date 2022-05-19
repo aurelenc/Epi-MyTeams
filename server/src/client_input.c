9d2331bccc19dfacec1f8befe99bbc7901aeb68a
aurelenc <aurele.nicolas@epitech.eu>
2022-05-19 14:59:54 +0200

    [FIX] Fixed command detection

--- server/src/client_input.c
+++ server/src/client_input.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "circular_buffer.h"
+#include <stdio.h>
 
 void write_client_buff(client_sock_t *clients, int i, char *message)
 {
@@ -30,20 +31,24 @@ int get_command_params(char **dest, char *src)
     return i;
 }
 
-void exec_cmd(command_param_t *params)
+void exec_cmd(command_param_t *params, int cmd_id)
 {
-    commands[params->id].func(params);
+    commands[cmd_id].func(params);
 }
 
 void find_command(command_param_t *params)
 {
     bool command_found = false;
+    char buff[MAX_BUFF_SIZE];
 
     for (size_t i = 0; commands[i].func != NULL; i++) {
-        if (strncmp(params->clients[params->id].rbuf, commands[i].cmd,
+        printf("%s\n", params->clients[params->id].rbuf);
+        if (cbuff_pop(params->clients[params->id].rbuf, buff, MAX_BUFF_SIZE) != BUFFER_OK)
+            break;
+        printf("%s\t%s:%ld\n", buff, commands[i].cmd, strlen(commands[i].cmd));
+        if (strncmp(buff, commands[i].cmd,
             strlen(commands[i].cmd)) == 0) {
-            exec_cmd(params);
-            memset(params->clients[params->id].rbuf, 0, MAX_BUFF_SIZE);
+            exec_cmd(params, i);
             command_found = true;
             break;
         }
@@ -57,14 +62,16 @@ void handle_input(client_sock_t *clients, int id, server_t *server)
 {
     params_t params;
     command_param_t command_params;
+    char buff[MAX_BUFF_SIZE];
 
     params.nb = 0;
     params.array = calloc(sizeof(char *), MAX_PARAMS_NB + 1);
     if (!params.array ||
-        read(clients[id].socket, clients[id].rbuf, MAX_BUFF_SIZE) == 0) {
+        read(clients[id].socket, buff, MAX_BUFF_SIZE) == 0) {
         remove_client(clients, id);
         return;
     }
+    cbuff_push(clients[id].rbuf, buff);
     params.nb = get_command_params(params.array, clients[id].rbuf);
     command_params.clients = clients;
     command_params.id = id;
