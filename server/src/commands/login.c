f73b94d96c820f9e312396799d2ad47dfecba3a9
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-05-30 17:52:04 +0200

    [ADD] login command send good reply

--- server/src/commands/login.c
+++ server/src/commands/login.c
@@ -10,16 +10,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "user.h"
-#include "database_users_add.h"
+#include "tables/users/user.h"
+#include "tables/users/database_users_add.h"
 
-static char *get_reply_msg(char *user_uuid)
+static char *get_reply_msg(char *user_uuid, char *user_name)
 {
-    char *reply = calloc(sizeof(char), strlen(user_uuid) + 3);
+    char *reply = calloc(sizeof(char), strlen(user_uuid) + 9);
 
-    strcat(reply, "[");
+    strcat(reply, " [\"");
     strcat(reply, user_uuid);
-    strcat(reply, "]");
+    strcat(reply, "\" \"");
+    strcat(reply, user_name);
+    strcat(reply, "\"]");
     return (reply);
 }
 
@@ -27,6 +29,7 @@ int command_login(command_param_t *param)
 {
     char success_buff[MAX_BUFF_SIZE] = {0};
     char *reply = 0;
+    user_t *user = 0;
 
     printf("[SERVER] LOGIN\n");
     if (param->arg.nb < 2) {
@@ -34,9 +37,10 @@ int command_login(command_param_t *param)
     } else if (param->arg.nb > 2) {
         return client_reply(param->clients, param->id, INVALID_FORMAT);
     }
-    db_add_user(param->srv->db,
-    user_init(llist_get_size(param->srv->db->users), param->arg.array[1], ""));
-    reply = get_reply_msg(((user_t *)llist_back(param->srv->db->users))->uuid);
+    user =
+    user_init(llist_get_size(param->srv->db->users), param->arg.array[1], "");
+    db_add_user(param->srv->db, user);
+    reply = get_reply_msg(user->uuid, user->pseudo);
     sprintf(success_buff, reply_codes[get_reply(SUCCESS)].message, reply);
     free(reply);
     return client_reply_success(param->clients, param->id, success_buff);
