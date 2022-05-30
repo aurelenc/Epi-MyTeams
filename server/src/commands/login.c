2bc7a54f403bf3523a923b91229918e364f53b44
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-05-30 17:44:32 +0200

    [IMP] login command can now add an user to the server's database

--- server/src/commands/login.c
+++ server/src/commands/login.c
@@ -8,12 +8,25 @@
 #include "reply_codes.h"
 #include "server.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "user.h"
+#include "database_users_add.h"
+
+static char *get_reply_msg(char *user_uuid)
+{
+    char *reply = calloc(sizeof(char), strlen(user_uuid) + 3);
+
+    strcat(reply, "[");
+    strcat(reply, user_uuid);
+    strcat(reply, "]");
+    return (reply);
+}
 
 int command_login(command_param_t *param)
 {
-    // char *user_uid;
-    // char reply[MAX_BUFF_SIZE];
     char success_buff[MAX_BUFF_SIZE] = {0};
+    char *reply = 0;
 
     printf("[SERVER] LOGIN\n");
     if (param->arg.nb < 2) {
@@ -21,9 +34,10 @@ int command_login(command_param_t *param)
     } else if (param->arg.nb > 2) {
         return client_reply(param->clients, param->id, INVALID_FORMAT);
     }
-    sprintf(success_buff, reply_codes[get_reply(SUCCESS)].message, "aaaa");
+    db_add_user(param->srv->db,
+    user_init(llist_get_size(param->srv->db->users), param->arg.array[1], ""));
+    reply = get_reply_msg(((user_t *)llist_back(param->srv->db->users))->uuid);
+    sprintf(success_buff, reply_codes[get_reply(SUCCESS)].message, reply);
+    free(reply);
     return client_reply_success(param->clients, param->id, success_buff);
-    // param->clients[param->id].user = param->arg.array[1];
-    // server_event_user_logged_in(user_uid)
-    // write_client_buff(param->clients, param->id, "777\n");
 }
