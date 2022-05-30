7a85647bd31082096598a7af36722d222c86d35e
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-05-30 21:56:09 +0200

    [IMP] users can now reply with the good reply

--- server/src/commands/users.c
+++ server/src/commands/users.c
@@ -7,8 +7,34 @@
 
 #include "reply_codes.h"
 #include "server.h"
+#include "tables/users/user.h"
+
+static void fill_msg_reply(llist_t *users, char *buff[MAX_BUFF_SIZE])
+{
+    node_t *ptr = users->first;
+
+    strcat(*buff, "00 ");
+    while (ptr && strlen(*buff) < MAX_BUFF_SIZE) {
+        strcat(*buff, "[ \"");
+        strcat(*buff,   ((user_t *)ptr)->uuid);
+        strcat(*buff, "\" \"");
+        strcat(*buff, ((user_t *)ptr)->pseudo);
+        strcat(*buff, "\" \"");
+        strcat(*buff, "0" );
+        strcat(*buff, "\"]");
+        ptr = ptr->next;
+    }
+}
 
 int command_users(command_param_t *param)
 {
-    return NOT_IMPLEMENTED;
+    char success_buff[MAX_BUFF_SIZE] = {0};
+    char *reply = 0;
+
+    printf("[SERVER] USERS\n");
+    if (param->arg.nb > 1) {
+        return client_reply(param->clients, param->id, INVALID_FORMAT);
+    }
+    fill_msg_reply(param->srv->db->users, &success_buff);
+    return client_reply_success(param->clients, param->id, success_buff);
 }
