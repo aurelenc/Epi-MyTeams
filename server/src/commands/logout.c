2ffbb688b47d5c274d6fce86c862bd402a1baa2b
aurelenc <aurele.nicolas@epitech.eu>
2022-06-01 18:14:08 +0200

    [FIX] Login command

--- server/src/commands/logout.c
+++ server/src/commands/logout.c
@@ -8,10 +8,12 @@
 #include "reply_codes.h"
 #include "server.h"
 #include <stdio.h>
+#include "logging_server.h"
 
 int command_logout(command_param_t *param)
 {
     printf("[SERVER] LOGIN\n");
     server_event_user_logged_out(param->clients[param->id].user);
     remove_client(param->clients, param->id);
+    return SUCCESS;
 }
