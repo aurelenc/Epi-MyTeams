2ee99efa905c1f95cbb40de87e5f0f59ce6ae8d9
aurelenc <aurele.nicolas@epitech.eu>
2022-06-02 19:29:20 +0200

    [REF] Optimized team verification on /use

--- server/src/commands/use_team.c
+++ server/src/commands/use_team.c
@@ -10,27 +10,25 @@
 #include "tables/teams/database_teams_search.h"
 #include "tables/users_x_teams/database_users_x_teams_multiple_search.h"
 #include "tables/users/user_search.h"
+#include "tables/users_x_teams/database_users_x_teams_search.h"
+#include "tables/users/database_users_search.h"
 #include <string.h>
 
 int command_use_team(command_param_t *param)
 {
     team_t *team = 0;
-    database_t *new = 0;
-    node_t *iterator = 0;
+    id_pair_t pair;
+    id_pair_t *search = 0;
 
     if (param->arg.nb < 2)
         return client_reply(param->clients, param->id, MISSING_PARAMETER);
     team = db_search_team_by_uuid(param->srv->db, param->arg.array[1]);
     if (!team)
         return client_reply(PARAM_CID, NOT_FOUND);
-    new = db_search_users_of_a_team_by_its_id(param->srv->db, team->id);
-    iterator = new->users->first;
-    while (iterator) {
-        if (user_uuid_compare(iterator->data, param->clients[param->id].user)) {
-            THIS_CLIENT.team_id = team->id;
-            return client_reply_success(PARAM_CID, "");
-        }
-        iterator = iterator->next;
-    }
-    return client_reply(PARAM_CID, FORBIDDEN);
+    pair.team_id = team->id;
+    pair.user_id = db_search_user_by_uuid(param->srv->db, THIS_CLIENT.user)->id;
+    search = db_search_user_team_by_pair(param->srv->db, &pair);
+    if (!search)
+        return client_reply(PARAM_CID, FORBIDDEN);
+    return client_reply_success(PARAM_CID, "");
 }
