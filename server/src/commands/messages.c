088fc70b8240c9ff0a0a967b77ef98c925368d9e
aurelenc <aurele.nicolas@epitech.eu>
2022-06-02 19:21:35 +0200

    [FIX] Crash on message command

--- server/src/commands/messages.c
+++ server/src/commands/messages.c
@@ -38,12 +38,17 @@ command_param_t *param)
     id_t user_pair[3] = {user_one->id, user_two->id, 0};
     discussion_t *disc =
     db_search_discussion_by_users_id(param->srv->db, user_pair);
-    database_t *new =
-    db_multiple_search_msg_by_discussion_id(param->srv->db, disc->id);
-    node_t *iterator = new->messages->first;
+    database_t *new = 0;
+    node_t *iterator = 0;
 
+    if (!disc)
+        return client_reply_success(PARAM_CID, "");
+    new = db_multiple_search_msg_by_discussion_id(param->srv->db, disc->id);
+    if (!new)
+        return client_reply_success(PARAM_CID, "");
+    iterator = new->messages->first;
     if (!iterator)
-        return client_reply_success(param->clients, param->id, "\n");
+        return client_reply_success(param->clients, param->id, "");
     write_client_buff(param->clients, param->id, "00:");
     fill_message(param, iterator);
     write_client_buff(param->clients, param->id, "\n");
