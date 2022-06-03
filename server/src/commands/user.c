c222e300e6078bf2414baae1b0a728b03e5bbcbe
aurelenc <aurele.nicolas@epitech.eu>
2022-06-03 15:52:57 +0200

    [REM] Remove stupid component

--- server/src/commands/user.c
+++ server/src/commands/user.c
@@ -11,8 +11,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "components/command_head.h"
-
 static char *is_user_connected(client_sock_t *clients, unsigned int user_id)
 {
     for (int i = 0; i < MAX_CLIENTS; i++) {
@@ -40,7 +38,17 @@ static char *get_msg_reply(user_t *user, client_sock_t *clients)
 
 int command_user(TEAMS_A)
 {
-    COMMAND_HEAD("USERS", 2, 2)
+    char *success_buff = 0;
+    user_t *found = 0;
+
+    printf("[SERVER] USER\n");
+    if (param->arg.nb < 2) {
+        return client_reply(PARAM_CID, MISSING_PARAMETER);
+    } else if (param->arg.nb > 2) {
+        return client_reply(PARAM_CID, INVALID_FORMAT);
+    }
+    if (!THIS_CLIENT.user)
+        return client_reply(PARAM_CID, FORBIDDEN);
     found = db_search_user_by_uuid(THIS_DB, THIS_ARG[1]);
     if (!found) {
         return client_reply(PARAM_CID, NOT_FOUND);
