e361bddb21ffeb49778a55c6340c7cb942148a63
Tom Duval-Mahe <tomduvalmahe44@gmail.com>
2022-06-03 11:21:26 +0200

    Merge

--- client/src/get_commands/get_cmd_users.c
+++ client/src/get_commands/get_cmd_users.c
@@ -13,25 +13,21 @@
 int users(char *av, int socket)
 {
     char code_response[3];
-    char **tab_response = NULL;
+    char **tab_res = NULL;
 
     if (check_params(av) == 0)
-        tab_response = send_command(av, tab_response, "USRS ", socket);
+        tab_res = send_command(av, tab_res, "USRS ", socket);
     else {
         printf("Command are not good use /help for more information !\n");
         return -1;
     }
-    strncpy(code_response, tab_response[0], 2);
+    strncpy(code_response, tab_res[0], 2);
     code_response[2] = '\0';
     if (!strcmp(code_response, "13"))
         client_error_unauthorized();
-
-    if (!strcmp(code_response, "40")) // @timestamp
-        client_print_users(tab_response[1], tab_response[3], atoi(tab_response[5]));
-    // char const *user_uuid,
-    // char const *user_name,
-    // int user_status);
-
-    free(tab_response);
+    if (!strcmp(code_response, "00")) //print_list_of_user
+        for (int i = 0; tab_res[i + 6]; i = i + 7)
+            client_print_users(tab_res[i + 1], tab_res[i + 3], atoi(tab_res[i + 5]));
+    free(tab_res);
     return 0;
 }
