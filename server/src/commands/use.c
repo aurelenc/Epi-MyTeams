1a9e89e10bed4b5644661118ed71bb3c48c4434d
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-06-04 16:24:57 +0200

    [FIX] required arg number

--- server/src/commands/use.c
+++ server/src/commands/use.c
@@ -13,13 +13,13 @@ int command_use(TEAMS_A)
 {
     if (!THIS_CLIENT.user)
         return client_reply(PARAM_CID, FORBIDDEN, EMPTY_REPLY);
-    if (param->arg.nb == 0)
-        return command_use_nothing(param);
     if (param->arg.nb == 1)
-        return command_use_one_arg(param);
+        return command_use_nothing(param);
     if (param->arg.nb == 2)
-        return command_use_two_arg(param);
+        return command_use_one_arg(param);
     if (param->arg.nb == 3)
+        return command_use_two_arg(param);
+    if (param->arg.nb == 4)
         return command_use_three_arg(param);
     return client_reply(PARAM_CID, INVALID_FORMAT, EMPTY_REPLY);;
 }
